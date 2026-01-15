9dc994fb390e974fb2159233c23559992bc9be3a
Zailio Labs <100409086+zailio-labs@users.noreply.github.com>
2026-01-16 03:15:42 +0530

    Update AllOrders.js

--- client/src/components/admin/orders/AllOrders.js
+++ client/src/components/admin/orders/AllOrders.js
@@ -28,7 +28,7 @@ const AllCategory = (props) => {
           <path
             strokeLinecap="round"
             strokeLinejoin="round"
-            strokeWidth={2}  {/* Fixed: changed from strokeWidth="2" to strokeWidth={2} */}
+            strokeWidth={2}
             d="M4 4v5h.582m15.356 2A8.001 8.001 0 004.582 9m0 0H9m11 11v-5h-.581m0 0a8.003 8.003 0 01-15.357-2m15.357 2H15"
           ></path>
         </svg>
@@ -187,7 +187,7 @@ const CategoryTable = ({ order, editOrder }) => {
               <path
                 strokeLinecap="round"
                 strokeLinejoin="round"
-                strokeWidth={2}  {/* Already correct */}
+                strokeWidth={2}
                 d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16"
               />
             </svg>
