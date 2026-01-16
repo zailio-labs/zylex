a7eeee17bee021c68cfcbf1b6713332bd4202797
Zailio Labs <100409086+zailio-labs@users.noreply.github.com>
2026-01-17 00:19:21 +0530

    Update braintree.js

--- server/routes/braintree.js
+++ server/routes/braintree.js
@@ -2,7 +2,8 @@ const express = require("express");
 const router = express.Router();
 const brainTreeController = require("../controller/braintree");
 
-router.post("/braintree/get-token", brainTreeController.ganerateToken);
+// FIX: Changed "ganerateToken" to "generateToken"
+router.post("/braintree/get-token", brainTreeController.generateToken);
 router.post("/braintree/payment", brainTreeController.paymentProcess);
 
 module.exports = router;
